8d9e14d3a61d647eb54e7865cec043820efdf4c1
juanmonto5 <juanmonto5@gmail.com>
2016-07-07 15:21:21 -0500

    avances

--- clases.rb
+++ clases.rb
@@ -11,11 +11,26 @@ class Person
 	def multiplicar (a,b)
 		"resultado #{a*b}"
 	end
+	def name
+		@name
+	end
+	def name= (new_name)
+		@name =new_name
+	end
+	def age
+		@name
+	end
+	def age= (new_age)
+		@age=new_age	
+	end
 end
 person1 = Person.new("Joe", 14)
 person2 = Person.new("Jose",20)
-p person1.instance_variables
+# p person1.instance_variables
+# puts person1.get_info
+# puts person2.get_info
+# p person1.instance_variables
+# puts person1.multiplicar(3,5)
+person1.age= 15 
+person1.name="Pedro"
 puts person1.get_info
-puts person2.get_info
-p person1.instance_variables
-puts person1.multiplicar(3,5)
